3155aaec42c0895a9920c912ab368aa69231a469
Ganesh <seelaboina.gani@gmail.com>
2024-12-18 16:41:37 +0530

    nodejs_docker

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-// Print a message to the console
-console.log('Hello, this is a simple Node.js script!');
+// Import the http module to create a server
+const http = require('http');
+
+// Define the port number
+const PORT = 3000;
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the status code and content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Send the response message
+  res.end('Hello, this is a simple Node.js server!');
+
+  // Log a message to the console when a request is received
+  console.log('A request was made to the server!');
+});
+
+// Start the server and listen on the specified port
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
