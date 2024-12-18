6be904d87c562bbf4ca55cc06aafede3669b067b
Ganesh <seelaboina.gani@gmail.com>
2024-12-18 13:37:03 +0530

    python_nodejs_docker_sample

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-// Import the HTTP module
-const http = require('http');
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  
-  // Send the response message
-  res.end('Hello, welcome to my Node.js server!');
-});
-
-// Define a port for the server
-const PORT = 3000;
-
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+// Print a message to the console
+console.log('Hello, this is a simple Node.js script!');
